6736183283d6fed567882f78558785380e117e67
phuonganhtuan1998@gmail.com <phuonganhtuan1998@gmail.com>
2020-03-13 18:40:12 +0700

    time added

---
+++ app/src/main/java/com/example/trashdetector/utils/TimeUtils.kt
@@ -0,0 +1,19 @@
+package com.example.trashdetector.utils
+
+import java.util.*
+
+object TimeUtils {
+
+    fun timeToString(millis: Long): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = millis
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val month = calendar.get(Calendar.MONTH).plus(1)
+        val year = calendar.get(Calendar.YEAR)
+        val time = String.format("%02d:%02d", hour, minute)
+        return "$time, $day-$month-$year"
+    }
+
+}
